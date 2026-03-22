491bfe002e847a3383f86d85391a34d1de6c7f22
SAFISA Local <local@safisa.invalid>
2026-03-22 10:10:51 -0300

    chore: add operational machines

--- database/seed_maquinas.sql
+++ database/seed_maquinas.sql
@@ -8,6 +8,11 @@ INSERT INTO maquinas (
   nome,
   tipo
 ) VALUES
-  (1, 'CNC', 'USINAGEM');
+  (1, 'CNC', 'USINAGEM'),
+  (2, 'FRESA FERRAMENTEIRA', 'FRESA'),
+  (3, 'TORNO CNC 01', 'TORNO CNC'),
+  (4, 'TORNO CNC 02', 'TORNO CNC'),
+  (5, 'TORNO CNC 03', 'TORNO CNC'),
+  (6, 'TORNO CONVENCIONAL', 'TORNO');
 
-ALTER TABLE maquinas AUTO_INCREMENT = 2;
+ALTER TABLE maquinas AUTO_INCREMENT = 7;
